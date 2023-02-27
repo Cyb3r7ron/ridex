6edb17099492518bc265cc463c342f26ae24e6fa
Cyb3r7ron <kiduskyle@gmail.com>
2023-02-27 16:49:30 -0500

    added marker

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-//import 'package:flutter/src/widgets/container.dart';
-//import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -14,56 +9,51 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  // in the below line, we are initializing our controller for google maps.
-  final Completer<GoogleMapController> _controller = Completer();
+  LatLng initialLocation = const LatLng(37.422131, -122.084801);
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
 
-  // in the below line, we are specifying our camera position
-  static const CameraPosition _kGoogle = CameraPosition(
-    target: LatLng(37.42796133580664, -122.885749655962),
-    zoom: 14.4746,
-  );
+  @override
+  void initState() {
+    addCustomIcon();
+    super.initState();
+  }
+
+  void addCustomIcon() {
+    BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(), "assets/Location_marker.png")
+        .then(
+      (icon) {
+        setState(() {
+          markerIcon = icon;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // in the below line, we are specifying our app bar.
-        appBar: AppBar(
-          // setting background color for app bar
-          backgroundColor: const Color(0xFFE4F311),
-          // setting title for app bar.
-          title: const Text("Ride"),
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: GestureDetector(
-              onTap: _makingPhoneCall,
-              child: const Icon(
-                Icons.phone, // add custom icons also
-              ),
-            ),
-          ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: initialLocation,
+          zoom: 14,
         ),
-        body: GoogleMap(
-          // in the below line, setting camera position
-          initialCameraPosition: _kGoogle,
-          // in the below line, specifying map type.
-          mapType: MapType.normal,
-          // in the below line, setting user location enabled.
-          myLocationEnabled: true,
-          // in the below line, setting compass enabled.
-          compassEnabled: true,
-          // in the below line, specifying controller on map complete.
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-        ));
-  }
-}
-
-_makingPhoneCall() async {
-  var url = Uri.parse("tel:+251931587237");
-  if (await canLaunchUrl(url)) {
-    await launchUrl(url);
-  } else {
-    throw 'Could not launch $url';
+        markers: {
+          Marker(
+            markerId: const MarkerId("marker1"),
+            position: const LatLng(37.422131, -122.084801),
+            draggable: true,
+            onDragEnd: (value) {
+              // value is the new position
+            },
+            icon: markerIcon,
+          ),
+          Marker(
+            markerId: const MarkerId("marker2"),
+            position: const LatLng(37.415768808487435, -122.08440050482749),
+          ),
+        },
+      ),
+    );
   }
 }
